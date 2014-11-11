c430f267fb09848ba2f652291f521f709567c5ff
Koray Balci <koray.balci@ccyazilim.com>
2014-11-11 18:23:13 +0200

    Basic version for iOS Vungle SDK version 3.0.10. Currently, config params are ignored and ad is played with default options for iOS.

--- src/ios/CordovaVungle.h
+++ src/ios/CordovaVungle.h
@@ -1,27 +1,13 @@
 #import <Foundation/Foundation.h>
 #import <Cordova/CDVPlugin.h>
-#import <vunglepub/vunglepub.h>
+#import <VungleSDK/VungleSDK.h>
 
-@interface CordovaVungle : CDVPlugin<VGVunglePubDelegate> {
+@interface CordovaVungle : CDVPlugin<VungleSDKDelegate> {
 
 }
 
-+ (void) init:(UIViewController*)vc appId:(NSString*)appId;
-+ (void) exit;
-    
-- (void) displayAdvert:(CDVInvokedUrlCommand*)command;
-- (void) displayIncentivizedAdvert:(CDVInvokedUrlCommand*)command;
+- (void) init:(CDVInvokedUrlCommand*)command;
+- (void) playAd:(CDVInvokedUrlCommand*)command;
 - (void) isVideoAvailable:(CDVInvokedUrlCommand*)command;
-   
-// VGVunglePubDelegate
-- (void)vungleMoviePlayed:(VGPlayData*)playData;
-/*
-- (void)vungleStatusUpdate:(VGStatusData*)statusData;
-*/
-- (void)vungleViewDidDisappear:(UIViewController*)viewController willShowProductView:(BOOL)willShow;
-- (void)vungleViewWillAppear:(UIViewController*)viewController;
-
-- (void)vungleAppStoreWillAppear;
-- (void)vungleAppStoreViewDidDisappear;
 
 @end
--- src/ios/CordovaVungle.m
+++ src/ios/CordovaVungle.m
@@ -1,109 +1,50 @@
 #import "CordovaVungle.h"
 
 @implementation CordovaVungle
-
-static UIViewController* viewController = nil;
-static NSString* mCallbackId = nil;
-static BOOL mPlayedVideoFully = FALSE;
     
-+ (void) init: (UIViewController*)vc appId:(NSString*)appId {
-    viewController = vc;
-    VGUserData*  data  = [VGUserData defaultUserData];
+- (void) init:(CDVInvokedUrlCommand*)command {
+    NSString* vungleId = nil;
+    if([command.arguments count] > 0 && [command.arguments objectAtIndex:0] != (id)[NSNull null]) {
+        vungleId = [command.arguments objectAtIndex:0];
+    } else {
+        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"no appId sent to init"];
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+        return;
+    }
     
-    // set up config data
-    //    data.age             = 36;
-    //    data.gender          = VGGenderFemale;
-    data.adOrientation   = VGAdOrientationPortrait;
-    data.locationEnabled = FALSE;
-    
+    VungleSDK* sdk = [VungleSDK sharedSDK];
     // start vungle publisher library
-    [VGVunglePub startWithPubAppID:appId userData:data];
-    [VGVunglePub allowAutoRotate:TRUE];
-    [VGVunglePub logToStdout:TRUE];
-    // [VGVunglePub (BOOL)setCustomCountDownText:(NSString*)text];
-    // [VGVunglePub (void)alertBoxWithTitle:(NSString*)title Body:(NSString*)body leftButtonTitle:(NSString*)button1 rightButtonTitle:(NSString*)button2];
+    [sdk startWithAppId:vungleId];
+    [sdk setDelegate:self];
     
-}
-    
-+ (void) exit {
-    [VGVunglePub stop];
-}
-    
-- (void) displayAdvert:(CDVInvokedUrlCommand*)command
-{
-    CDVPluginResult* pluginResult = nil;
-    if(viewController == nil) {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"no view to use"];
-    }
-    else {
-        [VGVunglePub playModalAd:viewController animated:TRUE];
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsInt:1];
-    }
+    NSLog(@"Vungle SDK: %@", VungleSDKVersion);
         
+    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
     
-- (void) displayIncentivizedAdvert:(CDVInvokedUrlCommand*)command
+- (void)dealloc {
+  [[VungleSDK sharedSDK] setDelegate:nil];
+}
+    
+- (void) playAd:(CDVInvokedUrlCommand*)command
 {
-    if(viewController == nil) {
-        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"no parse installation found"];
-        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    CDVPluginResult* pluginResult = nil;
+    if(self.viewController == nil) {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"no view to use"];
     }
     else {
-        mCallbackId = [[NSString alloc] initWithString:command.callbackId];
-        NSString* hrid = [command.arguments objectAtIndex:0];
-        [VGVunglePub setDelegate:self];
-        [VGVunglePub playIncentivizedAd:viewController animated:TRUE showClose:TRUE userTag:hrid];
+        [[VungleSDK sharedSDK] playAd:self.viewController];
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
     }
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
 - (void) isVideoAvailable:(CDVInvokedUrlCommand*)command
 {
-    CDVPluginResult* pluginResult = nil;
-	bool result = [VGVunglePub adIsAvailable];
-    
-    if (result) {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsInt:1];
-    } else { 
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"no video"];
-    }
-    
+    bool result = [[VungleSDK sharedSDK] isCachedAdAvailable];
+    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsBool:result];
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-}
-    
-// // VGVunglePubDelegate functions
-    
-- (void)vungleMoviePlayed:(VGPlayData*)playData
-{
-    mPlayedVideoFully = playData.playedFull;
-    //NSLog(@"movie played call");
-}
-    
-- (void)vungleAppStoreWillAppear
-{
-}
-
-- (void)vungleAppStoreViewDidDisappear
-{
-}
-
-- (void)vungleViewDidDisappear:(UIViewController*)viewController willShowProductView:(BOOL)willShow
-{
-    CDVPluginResult* pluginResult = nil;
-    if(mPlayedVideoFully) {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsInt:1];
-    } else {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsInt:0];
-    }
-    
-    if(mCallbackId != nil) {
-        [self.commandDelegate sendPluginResult:pluginResult callbackId:mCallbackId];
-    }
-}
-
-- (void)vungleViewWillAppear:(UIViewController*)viewController
-{
-    mPlayedVideoFully = FALSE;
 }
     
 @end
